dcd97a3bdcf8ab44cdf1ed174c4ab609df32fb1d
Jawdat Tayfour <18jwdat@gmail.com>
2023-08-20 00:35:22 +0300

    env var

--- portfolio_host/settings.py
+++ portfolio_host/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG",default=False)
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
